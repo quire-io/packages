7cfe1e2162e99c5db1ed11380e7c58d5da08d1d9
Rudy Huang <rudyhuang@potix.com>
2023-08-24 17:58:52 +0800

    [Quire] fix prevent picker from showing non-image files

--- packages/image_picker/image_picker_android/android/src/main/java/io/flutter/plugins/imagepicker/ImagePickerDelegate.java
+++ packages/image_picker/image_picker_android/android/src/main/java/io/flutter/plugins/imagepicker/ImagePickerDelegate.java
@@ -473,7 +473,7 @@ public class ImagePickerDelegate
                       .setMediaType(ActivityResultContracts.PickVisualMedia.ImageOnly.INSTANCE)
                       .build());
     } else {
-      pickImageIntent = new Intent(Intent.ACTION_GET_CONTENT);
+      pickImageIntent = new Intent(Intent.ACTION_PICK);
       pickImageIntent.setType("image/*");
     }
     activity.startActivityForResult(pickImageIntent, REQUEST_CODE_CHOOSE_IMAGE_FROM_GALLERY);
@@ -490,7 +490,7 @@ public class ImagePickerDelegate
                       .setMediaType(ActivityResultContracts.PickVisualMedia.ImageOnly.INSTANCE)
                       .build());
     } else {
-      pickMultiImageIntent = new Intent(Intent.ACTION_GET_CONTENT);
+      pickMultiImageIntent = new Intent(Intent.ACTION_PICK);
       pickMultiImageIntent.setType("image/*");
       pickMultiImageIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
     }
