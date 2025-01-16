7a7be809540e28388e2a209bdfb3f27c6a4a9e01
Rudy Huang <rudyhuang@potix.com>
2025-01-16 15:36:54 +0800

    [flutter_markdown] [Quire] blockquote color can be overridden

--- packages/flutter_markdown/lib/src/builder.dart
+++ packages/flutter_markdown/lib/src/builder.dart
@@ -497,7 +497,7 @@ class MarkdownBuilder implements md.NodeVisitor {
       child = _buildRichText(
         TextSpan(
           style: _isInBlockquote
-              ? styleSheet.blockquote!.merge(_inlines.last.style).copyWith(color: styleSheet.blockquote!.color)
+              ? styleSheet.blockquote!.merge(_inlines.last.style).copyWith(color: _inlines.last.tag == 'span' ? null : styleSheet.blockquote!.color)
               : _inlines.last.style,
           text: _isInBlockquote ? content : common.XmlUtil.decode(content), //trimText(text.text),
           recognizer: _linkHandlers.isNotEmpty ? _linkHandlers.last : null,
